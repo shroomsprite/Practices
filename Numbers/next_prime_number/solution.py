32505129a6710f4fc62c1fcc9d79be04624d120a
shroomsprite <martin.su928@gmail.com>
2018-06-06 16:10:02 +0800

    next prime number

---
+++ Numbers/next_prime_number/solution.py
@@ -0,0 +1,31 @@
+def main():
+    choice = input("First prime number? (0 for yes, 1 for stop): ")
+    num = 2
+    while True:
+        #print num
+        
+        if choice == 0:
+            if isPrime(num) == True:
+                print num
+                choice = input("Next prime number after " + str(num) + "? (0 for yes, 1 for stop): ")
+                num = num+1
+            else:
+                while not isPrime(num):
+                    num = num+1
+            
+        elif choice == 1:
+            break
+        else:
+            main()
+        #print num
+
+def isPrime(n):
+    flag=True
+    for i in range(2, (n/2)+1):
+        if n % i == 0:
+            flag=False
+            break
+    return flag
+    
+if __name__ == "__main__":
+    main()
